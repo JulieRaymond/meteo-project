0a51b4d1c44e70cf0aac3ee176857cb472bd293f
Jojopok <zaiajonathan@gmail.com>
2023-12-12 15:43:46 +0100

    its ok

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,40 +2,32 @@
 
 namespace App\Controller;
 
-use App\Service\WeatherService;
+use App\Service\OpenWeatherMapService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WeatherController
+class WeatherController extends AbstractController
 {
     private $weatherService;
 
-    public function __construct(WeatherService $weatherService)
+    public function __construct(OpenWeatherMapService $weatherService)
     {
         $this->weatherService = $weatherService;
     }
 
-    /**
-     * @Route("/", name="weather_index")
-     */
+    #[Route('/weather', name: 'weather')]
     public function index(Request $request): Response
     {
         $city = $request->query->get('city');
 
-        // Vérifiez si une ville a été sélectionnée
         if ($city) {
-            // Faites appel au service pour obtenir les données météo
-            $data = $this->weatherService->getWeatherByCity($city);
+            $weatherData = $this->weatherService->getWeatherForCity($city);
 
-            // Affichez les données dans votre template
-            return $this->render('weather/index.html.twig', [
-                'data' => $data,
-            ]);
+            return $this->render('weather/result.html.twig', ['weatherData' => $weatherData]);
         }
 
-        // Si aucune ville n'est sélectionnée, affichez simplement le formulaire
-        return $this->render('weather/form.html.twig');
+        return $this->render('weather/index.html.twig');
     }
 }
---
+++ src/Service/OpenWeatherMapService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class OpenWeatherMapService
+{
+    private $client;
+    private $apiKey;
+
+    public function __construct(HttpClientInterface $client, string $apiKey = 'acca7f148bfba703a2b03ab80db2b356')
+    {
+        $this->client = $client;
+        $this->apiKey = $apiKey;
+    }
+
+    public function getWeatherForCity(string $city): array
+    {
+        $response = $this->client->request('GET', 'http://api.openweathermap.org/data/2.5/weather', [
+            'query' => [
+                'q' => $city,
+                'appid' => $this->apiKey,
+            ],
+        ]);
+
+        return $response->toArray();
+    }
+}
